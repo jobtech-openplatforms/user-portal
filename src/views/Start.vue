b5a64786d803b274d16dd1a2d5f26820cb2737ff
Johanna Selin <johanna@codeisking.se>
2020-06-11 13:17:11 +0200

    restyled page

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="start page-content">
     <div class="login-container">
-      <div>
+      <div id="login-text">
         <h1>Get control over your <strong>reputation data</strong></h1>
+        <button class="button is-primary is-large" @click="onLogin()">Login</button>
         <p
           class="is-large"
         >The open platform API give you the ability to access your gig reputation data from the platforms where you have worked, and share it with any platform you want.</p>
-        <button class="button is-primary is-large" @click="onLogin()">Login</button>
       </div>
-      <div class="mobile-invisible">
+      <div id="login-img" class="mobile-invisible">
         <img
           class="open-platform-illustration"
           src="../assets/images/open-platforms-symbol.png"
@@ -20,47 +20,52 @@
 </template>
 
 <style lang="scss" scoped>
+@import "../assets/css/_variables";
+
+
+
 .login-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 10vh;
+  position:relative;
+  display:flex;
+  align-items:center;
+
+  
+  #login-text{
+    flex:0 0 520px;
+    padding-right:40px;
+  }
+
+  #login-img{
+    flex:1;
+  }
 
   button{
     display:block;
     margin:40px auto;
   }
 }
-.login-container div {
-  width: 50%;
-  margin-left: 10px;
-  margin-right: 10px;
-}
-.login-container div:first-child {
-  margin-left: 0;
-}
-.login-container div:last-child {
-  margin-right: 0;
-}
+
 .login-container .open-platform-illustration {
   width: 100%;
   height: auto;
 }
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: $narrow-screen) {
   .login-container {
-    flex-direction: column;
-    margin-top: 30px;
-    padding: 0 10px;
-  }
-  .login-container div:first-child {
-    margin-left: 10px;
-  }
-  .login-container div:last-child {
-    margin-right: 10px;
-  }
-  .login-container div {
-    width: 100%;
+    flex-direction:column;
+    text-align:center;
+
+    #login-text{
+      padding-right:0;
+      margin-bottom:40px;
+      flex:1 1 auto;
+    }
+
+    #login-img{
+      width:40vw;
+      max-width:400px;
+      margin:20px auto 10px;
+    }
   }
 }
 </style>
